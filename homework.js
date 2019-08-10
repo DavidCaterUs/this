18335399cc89a828daa900d66e7618c2a202b418
David Diaz <50279487+DavidCaterUs@users.noreply.github.com>
2019-08-10 01:11:57 -0300

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,126 @@
+console.log("it works ")
+
+function homework(data) {
+  console.log("Started solving HW")
+
+  var data = [
+
+    {
+
+      name: "Sergio",
+
+      age: 97,
+
+      color: "black"
+
+    },
+
+    {
+
+      name: "Carl",
+
+      age: 17,
+
+      color: "pink"
+
+    },
+
+    {
+
+      name: "Joseph",
+
+      age: 23,
+
+      color: "red"
+
+    },
+
+    {
+
+      name: "Joan",
+
+      age: 34,
+
+      color: "blue"
+
+    }
+
+    ,
+
+    {
+
+      name: "Terese",
+
+      age: 29,
+
+      color: "pink"
+
+    },
+
+    {
+
+      name: "Paul",
+
+      age: 47,
+
+      color: "black"
+
+    },
+
+    {
+
+      name: "Tyler",
+
+      age: 36,
+
+      color: "red"
+
+    }
+
+    ,
+
+    {
+
+      name: "Marian",
+
+      age: 31,
+
+      color: "orange"
+
+    }
+
+    ,
+
+    {
+
+      name: "Michael",
+
+      age: 35,
+
+      color: "green"
+
+    },
+
+    {
+
+      name: "Jay",
+
+      age: 51,
+
+      color: "pink"
+
+    }
+
+  ];
+
+  //1 - The name of the oldest user
+
+  //2 - The name of the youngest
+
+  //3 - the lest of different colors
+
+  //4- which color has more occurences
+
+
+  console.log("----------------------------------")
+}
